441f1669efe0901904bad6c446e1f908f63950ae
Lewis Cummins <lewis.cummins@triptease.com>
2025-08-25 10:53:23 +0100

    deploy registry first

--- infrastructure/container.tf
+++ infrastructure/container.tf
@@ -10,13 +10,6 @@ resource "google_service_account" "run_sa" {
   display_name = "Cloud Run runtime"
 }
 
-resource "google_artifact_registry_repository" "integrations_hub" {
-  project       = var.project_id
-  location      = "us"
-  repository_id = "integrations-hub"
-  format        = "DOCKER"
-}
-
 resource "google_artifact_registry_repository_iam_member" "puller" {
   project    = var.project_id
   location   = google_artifact_registry_repository.integrations_hub.location
---
+++ infrastructure/registry.tf
@@ -0,0 +1,6 @@
+resource "google_artifact_registry_repository" "integrations_hub" {
+  project       = var.project_id
+  location      = "us"
+  repository_id = "integrations-hub"
+  format        = "DOCKER"
+}
